78dbd7dad81a1df3c177783bb057799cf51ca7a5
Jonathan <48403308+mesielepush@users.noreply.github.com>
2019-11-07 18:36:41 -0600

    last changes

--- ruby_2.rb
+++ ruby_2.rb
@@ -2,6 +2,7 @@
 
 module Enumerable
   def my_each
+    return to_enum unless block_given?
     i = 0
     while i < size
       yield(self[i])
@@ -10,6 +11,7 @@ module Enumerable
   end
 
   def my_each_with_index
+    return to_enum unless block_given?
     ind = 0
     my_each do |x|
       yield(x, ind)
@@ -18,6 +20,7 @@ module Enumerable
   end
 
   def my_select
+    return to_enum unless block_given?
     selected = []
     my_each { |i| selected << i if yield(i) }
     selected
@@ -63,12 +66,31 @@ module Enumerable
     end
   end
 
-  def my_inject(obj = nil)
-    yielding = obj || shift
-    my_each do |x|
-      yielding = yield(yielding, x)
+  def my_inject(field = nil, second = nil) # rubocop:disable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
+    arr = to_a
+    o = nil
+
+    if field&.respond_to?(:is_a?) && field&.is_a?(Integer)
+      sum = field
+      d_m = 0
+      if second&.respond_to?(:is_a?) && second&.is_a?(Symbol)
+        o = second.to_s
+        o.sub! ':', ''
+      end
+    else
+      sum = arr[0]
+      d_m = 1
     end
-    yielding
+
+    if field&.respond_to?(:is_a?) && field&.is_a?(Symbol)
+      o = field.to_s
+      o.sub! ':', ''
+    end
+
+    (arr.length - d_m).times do |x|
+      sum = o ? sum.method(o).call(arr[x + d_m]) : sum = yield(sum, arr[x + d_m])
+    end
+    sum
   end
 
   def multiply_els(arr)
